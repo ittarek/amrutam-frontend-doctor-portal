2487818e61d617fc1708361f8c55c367f3cdcff4
Md Tariqul Islam <ittarek551@gmail.com>
2025-12-08 15:25:51 +0600

    fix the section title

--- src/components/Shared/SectionDesc.jsx
+++ src/components/Shared/SectionDesc.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const SectionDesc = ({title, desc}) => {
   return (
-      <div className='lg:w-[545px] text-center'>
+      <div className='lg:w-[545px] text-center lg:text-left'>
           <p className="font-['DM_Sans'] font-medium text-[28px] leading-none text-[#3A643C]  [&_span]:font-bold mb-4">{title}</p>
           <p className="font-['DM_Sans'] font-medium text-lg leading-[131%] text-[#666666]  [&_span]:font-bold">{desc}  </p>
     </div>
