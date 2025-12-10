ecc4578c97a84b06412106d7e5121a8d5ae6ec89
Md Tariqul Islam <ittarek551@gmail.com>
2025-12-10 21:38:21 +0600

    add vercel.jsno file

--- src/Pages/Home/FAQPage/FaqBanner.jsx
+++ src/Pages/Home/FAQPage/FaqBanner.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import bgImage from "@/assets/faq/banner.png";
+import { Search } from "lucide-react";
 // import { Input } from "@/components/ui/input"
 export const FaqBanner = () => {
     return (
@@ -11,7 +12,10 @@ export const FaqBanner = () => {
         >
             <div className="flex flex-col gap-6 pt-11 items-center">
                 <h1>FAQ</h1>
-                <input className="w-[400px]  py-3  px-3 rounded-lg bg-white" type="text" placeholder="Search for any queries that you have" />
+                <div className="flex items-center relative">
+                    <span className="absolute pl-2 mr-6"><Search /></span>
+                    <input className="w-[400px]  py-3  px-3 rounded-lg bg-white" type="text" placeholder="Search for any queries that you have" />
+                </div>
             </div>
         </div>
     );
